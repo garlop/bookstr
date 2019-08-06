dae126a60a67619ec8455f84c0fe424396163f67
Luis Alberto Garnica López <lagl95@hotmail.com>
2019-08-06 03:52:07 -0500

    Initial Setup of React application using create react app

--- app.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-fs.readFile('./package.json', function(err, data){
-    if(err){
-        return console.log('No se pudo abrir el archivo' + err.message)
-    }
-    console.log(data.toString().length)
-});
-console.log("Hola Platzi")
